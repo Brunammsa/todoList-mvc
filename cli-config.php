27f57f3a447b85ed61de2d9466474e257218b4a7
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-05-26 18:02:19 -0300

    configuração composer, gitignore com vendor e .env

---
+++ cli-config.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Bruna\TodoListMvc\Connection\ConnectionCreator;
+use Doctrine\Migrations\Configuration\Migration\PhpFile;
+use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
+use Doctrine\Migrations\DependencyFactory;
+
+$config = new PhpFile(__DIR__ . 'migrations.php');
+
+$entityManager = ConnectionCreator::createEntityManager()
+;
+return DependencyFactory::fromEntityManager($config, new ExistingEntityManager($entityManager));
