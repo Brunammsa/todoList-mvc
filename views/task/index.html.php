bf8d393ceb6ac6766a92d9905ab2053fadf28a97
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-05-27 18:23:19 -0300

    implementando bootstrap

--- views/task/index.html.php
+++ views/task/index.html.php
@@ -1,1 +1,18 @@
-<div> hello world </div>
+<?php
+require_once __DIR__ . '/../inicio-html.php';
+?>
+
+<div class="container mt-5">
+    <ul class="list-group">
+        <?php foreach($tasks as $task): ?>
+        <li class="list-group-item list-group-item-action d-flex justify-content-between">
+            <span>
+                <?= $task->name;?>
+            </span>
+            <button type="button" class="btn btn-danger">Remove</button>
+        </li>
+        <?php endforeach ?>
+    </ul>
+</div>
+
+<?php require_once __DIR__ . '/../fim-html.php';
