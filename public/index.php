c389e1dd18e9ce6a33cce7f5929aa7fe19feb62b
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-05-27 17:40:10 -0300

    modificando index

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+use Bruna\TodoListMvc\Connection\ConnectionCreator;
+use Bruna\TodoListMvc\Controller\Error404Controller;
+use Bruna\TodoListMvc\Entities\Task;
+
+require_once __DIR__ . './../vendor/autoload.php';
+
+$entityManager = ConnectionCreator::createEntityManager();
+$taskRepository = $entityManager->getRepository(Task::class);
+
+$routes = require_once __DIR__ . '/../config/routes.php';
+
+$pathInfo = $_SERVER['PATH_INFO'] ?? '/';
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+
+$key = "$httpMethod|$pathInfo";
+
+if (array_key_exists($key, $routes)) {
+    $controllerClasse = $routes["$httpMethod|$pathInfo"];
+
+    $controller = new $controllerClasse($taskRepository);
+} else {
+    $controller = new Error404Controller();
+}
+
+$controller->process();
