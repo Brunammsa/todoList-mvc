f75db7b6673c9e830e6a69fdfbefcc50c95e6c1e
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-05-29 17:03:51 -0300

    finalização das rotas

--- config/routes.php
+++ config/routes.php
@@ -1,7 +1,16 @@
 <?php
 
+use Bruna\TodoListMvc\Controller\FormController;
+use Bruna\TodoListMvc\Controller\TaskAddController;
 use Bruna\TodoListMvc\Controller\TaskListController;
+use Bruna\TodoListMvc\Controller\TaskRemoveController;
+use Bruna\TodoListMvc\Controller\TaskUpdateController;
 
 return [
-    'GET|/' => TaskListController::class
+    'GET|/' => TaskListController::class,
+    'GET|/new-task' =>FormController::class,
+    'POST|/new-task' => TaskAddController::class,
+    'GET|/remove-task' => TaskRemoveController::class,
+    'GET|/update-task' => FormController::class,
+    'POST|/update-task' => TaskUpdateController::class
 ];
