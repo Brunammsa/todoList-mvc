24be657bbe3288df98ec628d1a6d2a7c7d0b431c
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-05-30 16:57:52 -0300

    criando html para criação

--- views/task/index.html.php
+++ views/task/index.html.php
@@ -3,35 +3,31 @@ require_once __DIR__ . '/../inicio-html.php';
 ?>
 
 <div class="container mt-5">
+    <h2>Todo List</h2>
+
+    <form class="mb-3" action="/new-task" method="post">
+        <div class="form-group row">
+            <div class="col-11">
+                <input class="form-control" type="text" name="task-name" placeholder="Qual a próxima tarefa?">
+            </div>
+            <div class="col-1">
+                <input class="btn btn-success w-100" type="submit" value="Criar">
+            </div>
+        </div>
+    </form>
+    <hr>
+
     <ul class="list-group">
         <?php foreach ($tasks as $task) : ?>
             <li class="list-group-item list-group-item-action d-flex justify-content-between">
                 <span>
+                    <input type="checkbox">
                     <?= $task->name; ?>
                 </span>
                 <button type="button" class="btn btn-danger">Remove</button>
             </li>
         <?php endforeach ?>
     </ul>
-    <div class="modal" tabindex="-1">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>Modal body text goes here.</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
-
-
 
 <?php require_once __DIR__ . '/../fim-html.php';
