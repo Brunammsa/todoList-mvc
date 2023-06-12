f632794d22c64929def7af069d7ca8c24349242c
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-06-12 10:57:23 -0300

    implementação correta da psr17 recuperando a di

--- public/index.php
+++ public/index.php
@@ -2,19 +2,18 @@
 
 declare(strict_types=1);
 
-use Bruna\TodoListMvc\Connection\ConnectionCreator;
 use Bruna\TodoListMvc\Controller\Error404Controller;
-use Bruna\TodoListMvc\Entities\Task;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use Nyholm\Psr7Server\ServerRequestCreator;
 use Psr\Http\Message\ResponseInterface;
 
 require_once __DIR__ . './../vendor/autoload.php';
 
-$entityManager = ConnectionCreator::createEntityManager();
-$taskRepository = $entityManager->getRepository(Task::class);
-
 $routes = require_once __DIR__ . '/../config/routes.php';
+
+/** @var \Psr\Container\ContainerInterface $diContainer */
+$diContainer = require_once __DIR__ . '/../config/dependencies.php';
+
 
 $pathInfo = $_SERVER['PATH_INFO'] ?? '/';
 $httpMethod = $_SERVER['REQUEST_METHOD'];
@@ -37,7 +36,7 @@ if (array_key_exists($key, $routes)) {
     $controllerClasse = $routes[$key][0];
     $methodName = $routes[$key][1];
 
-    $controller = new $controllerClasse($taskRepository);
+    $controller = $diContainer->get($controllerClasse);
 } else {
     $controller = new Error404Controller();
 }
