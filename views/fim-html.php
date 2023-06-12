a5ab7ee94d20e0d483c41846ec09517606e4a654
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-06-12 12:16:29 -0300

    criou-se um arquivo layout com o início e o fim, inutilizando esses dois arquivos

--- views/fim-html.php
+++
@@ -1,6 +0,0 @@
-</body>
-    <script>
-        const alertList = document.querySelectorAll('.alert')
-        const alerts = [...alertList].map(element => new bootstrap.Alert(element))
-    </script>
-</html>
